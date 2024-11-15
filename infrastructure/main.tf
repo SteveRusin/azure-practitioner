8f557950225f1ced224da57b80a11c9ea28e6979
Stepan Sendun <Stepan_Sendun@epam.com>
2024-11-15 15:42:25 +0200

    comment all infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,123 +1,123 @@
-resource "azurerm_resource_group" "front_end_rg" {
-  name     = "rg-frontend-sand-ne-001"
-  location = var.location
-}
-
-resource "azurerm_storage_account" "front_end_storage_account" {
-  name     = "sandboxfrontend01"
-  location = var.location
-
-  account_replication_type = "LRS"
-  account_tier             = "Standard"
-  account_kind             = "StorageV2"
-  resource_group_name      = azurerm_resource_group.front_end_rg.name
-
-  static_website {
-    index_document     = "index.html"
-    error_404_document = "index.html"
-  }
-}
-
-resource "azurerm_resource_group" "product_service_rg" {
-  location = var.location
-  name     = "rg-product-service-sand-ne-001"
-}
-
-resource "azurerm_storage_account" "products_service_fa" {
-  name     = "stgsangproductsfane003"
-  location = var.location
-
-  account_replication_type = "LRS"
-  account_tier             = "Standard"
-  account_kind             = "StorageV2"
-
-  resource_group_name = azurerm_resource_group.product_service_rg.name
-}
-
-resource "azurerm_storage_share" "products_service_fa" {
-  name  = "fa-products-service-share"
-  quota = 2
-
-  storage_account_name = azurerm_storage_account.products_service_fa.name
-}
-
-resource "azurerm_service_plan" "product_service_plan" {
-  name     = "asp-product-service-sand-ne-001"
-  location = var.location
-
-  os_type  = "Windows"
-  sku_name = "Y1"
-
-  resource_group_name = azurerm_resource_group.product_service_rg.name
-}
-
-resource "azurerm_application_insights" "products_service_fa" {
-  name             = "appins-fa-products-service-sand-ne-001"
-  application_type = "web"
-  location         = var.location
-
-
-  resource_group_name = azurerm_resource_group.product_service_rg.name
-}
-
-
-resource "azurerm_windows_function_app" "products_service" {
-  name     = "fa-products-service-sand-ne-001"
-  location = var.location
-
-  service_plan_id     = azurerm_service_plan.product_service_plan.id
-  resource_group_name = azurerm_resource_group.product_service_rg.name
-
-  storage_account_name       = azurerm_storage_account.products_service_fa.name
-  storage_account_access_key = azurerm_storage_account.products_service_fa.primary_access_key
-
-  functions_extension_version = "~4"
-  builtin_logging_enabled     = false
-
-  site_config {
-    always_on = false
-
-    application_insights_key               = azurerm_application_insights.products_service_fa.instrumentation_key
-    application_insights_connection_string = azurerm_application_insights.products_service_fa.connection_string
-
-    use_32_bit_worker = true
-
-    cors {
-      allowed_origins = ["*"]
-    }
-
-    application_stack {
-      node_version = "~18"
-    }
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  app_settings = {
-    WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = azurerm_storage_account.products_service_fa.primary_connection_string
-    WEBSITE_CONTENTSHARE                     = azurerm_storage_share.products_service_fa.name
-    FUNCTIONS_WORKER_RUNTIME                 = "node"
-    DB_URI                                   = azurerm_cosmosdb_account.products_account.endpoint
-    DB_NAME                                  = azurerm_cosmosdb_sql_database.products_app.name
-    DOTNET_USE_POLLING_FILE_WATCHER          = 1
-    WEBSITE_RUN_FROM_PACKAGE                 = 1
-    SB_CONNECTION_STRING                     = azurerm_servicebus_namespace.service_bus.default_primary_connection_string
-    SB_PRODUCTS_IMPORT_TOPIC_OR_QUEUE        = var.topic_or_queue_name
-  }
-
-  lifecycle {
-    ignore_changes = [
-      site_config["application_stack"], // workaround for a bug when azure just "kills" your app
-      tags["hidden-link: /app-insights-instrumentation-key"],
-      tags["hidden-link: /app-insights-resource-id"],
-      tags["hidden-link: /app-insights-conn-string"]
-    ]
-  }
-}
-
-output "function_app_principal_id" {
-  value = azurerm_windows_function_app.products_service.identity[0].principal_id
-}
-
+# resource "azurerm_resource_group" "front_end_rg" {
+#   name     = "rg-frontend-sand-ne-001"
+#   location = var.location
+# }
+#
+# resource "azurerm_storage_account" "front_end_storage_account" {
+#   name     = "sandboxfrontend01"
+#   location = var.location
+#
+#   account_replication_type = "LRS"
+#   account_tier             = "Standard"
+#   account_kind             = "StorageV2"
+#   resource_group_name      = azurerm_resource_group.front_end_rg.name
+#
+#   static_website {
+#     index_document     = "index.html"
+#     error_404_document = "index.html"
+#   }
+# }
+#
+# resource "azurerm_resource_group" "product_service_rg" {
+#   location = var.location
+#   name     = "rg-product-service-sand-ne-001"
+# }
+#
+# resource "azurerm_storage_account" "products_service_fa" {
+#   name     = "stgsangproductsfane003"
+#   location = var.location
+#
+#   account_replication_type = "LRS"
+#   account_tier             = "Standard"
+#   account_kind             = "StorageV2"
+#
+#   resource_group_name = azurerm_resource_group.product_service_rg.name
+# }
+#
+# resource "azurerm_storage_share" "products_service_fa" {
+#   name  = "fa-products-service-share"
+#   quota = 2
+#
+#   storage_account_name = azurerm_storage_account.products_service_fa.name
+# }
+#
+# resource "azurerm_service_plan" "product_service_plan" {
+#   name     = "asp-product-service-sand-ne-001"
+#   location = var.location
+#
+#   os_type  = "Windows"
+#   sku_name = "Y1"
+#
+#   resource_group_name = azurerm_resource_group.product_service_rg.name
+# }
+#
+# resource "azurerm_application_insights" "products_service_fa" {
+#   name             = "appins-fa-products-service-sand-ne-001"
+#   application_type = "web"
+#   location         = var.location
+#
+#
+#   resource_group_name = azurerm_resource_group.product_service_rg.name
+# }
+#
+#
+# resource "azurerm_windows_function_app" "products_service" {
+#   name     = "fa-products-service-sand-ne-001"
+#   location = var.location
+#
+#   service_plan_id     = azurerm_service_plan.product_service_plan.id
+#   resource_group_name = azurerm_resource_group.product_service_rg.name
+#
+#   storage_account_name       = azurerm_storage_account.products_service_fa.name
+#   storage_account_access_key = azurerm_storage_account.products_service_fa.primary_access_key
+#
+#   functions_extension_version = "~4"
+#   builtin_logging_enabled     = false
+#
+#   site_config {
+#     always_on = false
+#
+#     application_insights_key               = azurerm_application_insights.products_service_fa.instrumentation_key
+#     application_insights_connection_string = azurerm_application_insights.products_service_fa.connection_string
+#
+#     use_32_bit_worker = true
+#
+#     cors {
+#       allowed_origins = ["*"]
+#     }
+#
+#     application_stack {
+#       node_version = "~18"
+#     }
+#   }
+#
+#   identity {
+#     type = "SystemAssigned"
+#   }
+#
+#   app_settings = {
+#     WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = azurerm_storage_account.products_service_fa.primary_connection_string
+#     WEBSITE_CONTENTSHARE                     = azurerm_storage_share.products_service_fa.name
+#     FUNCTIONS_WORKER_RUNTIME                 = "node"
+#     DB_URI                                   = azurerm_cosmosdb_account.products_account.endpoint
+#     DB_NAME                                  = azurerm_cosmosdb_sql_database.products_app.name
+#     DOTNET_USE_POLLING_FILE_WATCHER          = 1
+#     WEBSITE_RUN_FROM_PACKAGE                 = 1
+#     SB_CONNECTION_STRING                     = azurerm_servicebus_namespace.service_bus.default_primary_connection_string
+#     SB_PRODUCTS_IMPORT_TOPIC_OR_QUEUE        = var.topic_or_queue_name
+#   }
+#
+#   lifecycle {
+#     ignore_changes = [
+#       site_config["application_stack"], // workaround for a bug when azure just "kills" your app
+#       tags["hidden-link: /app-insights-instrumentation-key"],
+#       tags["hidden-link: /app-insights-resource-id"],
+#       tags["hidden-link: /app-insights-conn-string"]
+#     ]
+#   }
+# }
+#
+# output "function_app_principal_id" {
+#   value = azurerm_windows_function_app.products_service.identity[0].principal_id
+# }
+#
--- infrastructure/service-bus-queue.tf
+++ infrastructure/service-bus-queue.tf
@@ -1,29 +1,29 @@
-resource "azurerm_resource_group" "service_bus_rg" {
-  name     = "rg-service-bus-sand-ne-001"
-  location = var.location
-}
-
-resource "azurerm_servicebus_namespace" "service_bus" {
-  name                          = "sb-import-products"
-  location                      = azurerm_resource_group.service_bus_rg.location
-  resource_group_name           = azurerm_resource_group.service_bus_rg.name
-  sku                           = "Basic"
-  capacity                      = 0 /* standard for sku plan */
-  public_network_access_enabled = true /* can be changed to false for premium */
-  minimum_tls_version           = "1.2"
-  zone_redundant                = false /* can be changed to true for premium */
-}
-
-resource "azurerm_servicebus_queue" "import_products" {
-  name                                    = var.topic_or_queue_name
-  namespace_id                            = azurerm_servicebus_namespace.service_bus.id
-  status                                  = "Active"
-  enable_partitioning                     = true
-  lock_duration                           = "PT1M" /* ISO 8601 timespan duration, 5 min is max */
-  requires_duplicate_detection            = false
-  duplicate_detection_history_time_window = "PT10M" /* ISO 8601 timespan duration, 5 min is max */
-  requires_session                        = false
-  dead_lettering_on_message_expiration    = false
-}
-
-
+# resource "azurerm_resource_group" "service_bus_rg" {
+#   name     = "rg-service-bus-sand-ne-001"
+#   location = var.location
+# }
+#
+# resource "azurerm_servicebus_namespace" "service_bus" {
+#   name                          = "sb-import-products"
+#   location                      = azurerm_resource_group.service_bus_rg.location
+#   resource_group_name           = azurerm_resource_group.service_bus_rg.name
+#   sku                           = "Basic"
+#   capacity                      = 0 /* standard for sku plan */
+#   public_network_access_enabled = true /* can be changed to false for premium */
+#   minimum_tls_version           = "1.2"
+#   zone_redundant                = false /* can be changed to true for premium */
+# }
+#
+# resource "azurerm_servicebus_queue" "import_products" {
+#   name                                    = var.topic_or_queue_name
+#   namespace_id                            = azurerm_servicebus_namespace.service_bus.id
+#   status                                  = "Active"
+#   enable_partitioning                     = true
+#   lock_duration                           = "PT1M" /* ISO 8601 timespan duration, 5 min is max */
+#   requires_duplicate_detection            = false
+#   duplicate_detection_history_time_window = "PT10M" /* ISO 8601 timespan duration, 5 min is max */
+#   requires_session                        = false
+#   dead_lettering_on_message_expiration    = false
+# }
+#
+#
